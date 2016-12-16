b9e2add21db47c9f17532299fc612fe28f5630ce
Jeroen Ooms <jeroen.ooms@stat.ucla.edu>
2016-12-16 14:25:21 +0000

    version 1.1

--- R/wrappers.R
+++ R/wrappers.R
@@ -8,8 +8,9 @@
 #' @export
 #' @rdname jsonld
 #' @name jsonld
-#' @param doc string with JSON-LD document
-#' @param context string with JSON-LD context
+#' @param doc a URL or literal string with JSON-LD document
+#' @param context a URL or literal string with JSON-LD context
+#' @param options named list with advanced options
 #' @examples # Example from https://github.com/digitalbazaar/jsonld.js#quick-examples
 #' doc <- '{
 #'   "http://schema.org/name": "Manu Sporny",
@@ -34,61 +35,75 @@
 #' # Other utilities:
 #' jsonld_flatten(doc)
 #' cat(jsonld_normalize(doc))
-jsonld_compact <- function(doc, context){
-  if(!jsonlite::validate(doc))
-    stop("Argument 'doc' is not a valid JSON string")
-  if(!jsonlite::validate(context))
-    stop("Argument 'context' is not a valid JSON string")
-  structure(ctx$call("r_compact", V8::JS(doc), V8::JS(context)), class = "json")
+jsonld_compact <- function(doc, context, options = NULL){
+  doc <- validate_arg(doc)
+  context <- validate_arg(context)
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  structure(ctx$call("r_compact", doc, context, options), class = "json")
 }
 
 #' @export
 #' @rdname jsonld
-#' @param compacted string with JSON message
-jsonld_expand <- function(compacted){
-  if(!jsonlite::validate(compacted))
-    stop("Argument 'compacted' is not a valid JSON string")
-  structure(ctx$call("r_expand", V8::JS(compacted)), class = "json")
+#' @param compacted a URL or literal string with JSON message
+jsonld_expand <- function(compacted, options = NULL){
+  compacted <- validate_arg(compacted)
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  structure(ctx$call("r_expand", compacted, options), class = "json")
 }
 
 #' @export
 #' @rdname jsonld
-jsonld_flatten <- function(doc){
-  if(!jsonlite::validate(doc))
-    stop("Argument 'doc' is not a valid JSON string")
-  structure(ctx$call("r_flatten", V8::JS(doc)), class = "json")
+jsonld_flatten <- function(doc, context = NULL, options = NULL){
+  if(is.null(context))
+    context <- "null"
+  doc <- validate_arg(doc)
+  context <- validate_arg(context)
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  structure(ctx$call("r_flatten", doc, context, options), class = "json")
 }
 
 #' @export
 #' @rdname jsonld
-jsonld_frame <- function(doc){
-  if(!jsonlite::validate(doc))
-    stop("Argument 'doc' is not a valid JSON string")
-  structure(ctx$call("r_frame", V8::JS(doc)), class = "json")
+#' @param frame a URL or literal string with JSON-LD frame
+jsonld_frame <- function(doc, frame, options = NULL){
+  doc <- validate_arg(doc)
+  frame <- validate_arg(frame)
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  structure(ctx$call("r_frame", doc, frame, options), class = "json")
 }
 
 #' @export
 #' @rdname jsonld
 #' @param rdf string with RDF text
-jsonld_from_rdf <- function(rdf, format = 'application/nquads'){
+jsonld_from_rdf <- function(rdf, options = list(format = 'application/nquads')){
   stopifnot(is.character(rdf))
-  structure(ctx$call("r_from_rdf", rdf, format), class = "json")
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  structure(ctx$call("r_from_rdf", rdf, options), class = "json")
 }
 
 #' @export
 #' @rdname jsonld
-jsonld_to_rdf <- function(doc, format = 'application/nquads'){
-  if(!jsonlite::validate(doc))
-    stop("Argument 'doc' is not a valid JSON string")
-  ctx$call("r_to_rdf", V8::JS(doc), format)
+jsonld_to_rdf <- function(doc, options = list(format = 'application/nquads')){
+  doc <- validate_arg(doc)
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  ctx$call("r_to_rdf", doc, options)
 }
 
 #' @export
 #' @rdname jsonld
-#' @param algorithm normalization algorithm
-#' @param format a MIME type
-jsonld_normalize <- function(doc, algorithm = 'URDNA2015', format = 'application/nquads'){
-  if(!jsonlite::validate(doc))
-    stop("Argument 'doc' is not a valid JSON string")
-  ctx$call("r_normalize", V8::JS(doc), algorithm, format)
+jsonld_normalize <- function(doc, options = list(algorithm = 'URDNA2015', format = 'application/nquads')){
+  doc <- validate_arg(doc)
+  options <- V8::JS(jsonlite::toJSON(options, auto_unbox = TRUE))
+  ctx$call("r_normalize", doc, options)
+}
+
+# Check if argument is a JSON string or URL
+validate_arg <- function(x){
+  if(is.character(x) && length(x) == 1){
+    if(jsonlite::validate(x))
+      return(V8::JS(x))
+    if(grepl("^https?://", x))
+      return(x)
+  }
+  stop("Argument is not a valid URL or JSON string")
 }
--- inst/js/wrappers.js
+++ inst/js/wrappers.js
@@ -32,89 +32,86 @@ jsonld.documentLoaders.curl = function(options){
 
 jsonld.useDocumentLoader('curl');
 
-function r_compact(doc, context){
+function r_compact(doc, context, options){
   var err;
   var compacted;
-  jsonld.compact(doc, context, function(x, y) {
+  jsonld.compact(doc, context, options, function(x, y) {
     err = x;
     compacted = y;
   });
   if(err) throw err;
-  if(!compacted) throw "Invalid input";
+  if(compacted === undefined) throw "Invalid input";
   return JSON.stringify(compacted, null, 2);
 }
 
-function r_expand(compacted){
+function r_expand(compacted, options){
   var err;
   var expanded;
-  jsonld.expand(compacted, function(x, y) {
+  jsonld.expand(compacted, options, function(x, y) {
     err = x;
     expanded = y;
   });
   if(err) throw err;
-  if(!expanded) throw "Invalid input";
+  if(expanded === undefined) throw "Invalid input";
   return JSON.stringify(expanded, null, 2);
 }
 
-function r_flatten(doc){
+function r_flatten(doc, context, options){
   var err;
   var flattened;
-  jsonld.flatten(doc, function(x, y) {
+  jsonld.flatten(doc, context, options, function(x, y) {
     err = x;
     flattened = y;
   });
   if(err) throw err;
-  if(!flattened) throw "Invalid input";
+  if(flattened === undefined) throw "Invalid input";
   return JSON.stringify(flattened, null, 2);
 }
 
-function r_frame(doc, frame){
+function r_frame(doc, frame, options){
   var err;
   var framed;
-  jsonld.frame(doc, frame, function(x, y) {
+  jsonld.frame(doc, frame, options, function(x, y) {
     err = x;
     framed = y;
   });
   if(err) throw err;
-  if(!framed) throw "Invalid input";
+  if(framed === undefined) throw "Invalid input";
   return JSON.stringify(framed, null, 2);
 }
 
-function r_normalize(doc, algorithm, format){
+function r_normalize(doc, options){
   var err;
   var normalized;
-  jsonld.normalize(doc, {
-    algorithm: algorithm,
-    format: format
-  }, function(x, y) {
+  jsonld.normalize(doc, options, function(x, y) {
     err = x;
     normalized = y;
   });
   if(err) throw err;
-  if(!normalized) throw "Invalid input";
+  if(normalized === undefined) throw "Invalid input";
   return normalized;
 }
 
-function r_to_rdf(doc, format){
+function r_to_rdf(doc, options){
   var err;
   var nquads;
-  jsonld.toRDF(doc, {format : format}, function(x, y) {
+  jsonld.toRDF(doc, options, function(x, y) {
     err = x;
     nquads = y;
   });
   if(err) throw err;
-  if(!nquads) throw "Invalid input";
+  if(nquads === undefined) throw "Invalid input";
   return nquads;
 }
 
-function r_from_rdf(text, format){
+function r_from_rdf(text, options){
   var err;
   var doc;
-  jsonld.fromRDF(text, {format : format}, function(x, y) {
+  jsonld.fromRDF(text, options, function(x, y) {
     err = x;
     doc = y;
   });
   if(err) throw err;
-  if(!doc) throw "Invalid input";
+  if(doc === undefined) throw "Invalid input";
   return JSON.stringify(doc, null, 2);
 }
